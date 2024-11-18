8e8967680dc0c48e5cfffff548d8a0abca5e9847
6161shaurya <shauryaj.2004@gmail.com>
2024-11-18 10:30:53 +0530

    netflix

---
+++ Day10/netflix-clone/src/App.jsx
@@ -0,0 +1,12 @@
+import MovieList from "./MoviesList";
+
+const App=()=>{
+  return(
+    <div> 
+    
+      <h1 style={{backgroundColor:"black",color:"White", textAlign:"center"}}>Netflix-Clone App</h1>
+      <MovieList/>
+    </div>
+  )
+}
+export default App;
---
+++ Day10/netflix-clone/src/movies.js
@@ -0,0 +1,37 @@
+const movies=[{
+    id:1,
+    image:"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/4/f/r/2009-dvd-eagle-hindi-tarzan-the-wonder-car-original-imahfdpjpqg85j5d.jpeg?q=70",
+    title: "Tarzan-The Wonder Car",
+    language:"Hindi",
+    cost:290,
+},
+{
+    id:2,
+    _image:"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/l/t/h/2022-blu-ray-20th-century-studios-english-avatar-2-original-imagvyqpzps5edgz.jpeg?q=70",
+    title: "Avatar 2",
+    language:"English",
+    cost:1500,
+},
+{
+    id:3,
+    image:"https://rukminim2.flixcart.com/image/612/612/jm2c87k0/movie/j/v/m/2018-blu-ray-20th-century-fox-excel-home-videos-english-war-for-original-imaf9yvgpedf2wxj.jpeg?q=70",
+    title: "War of the planet of the apes",
+    language:"English",
+    cost:600,
+},
+{
+    id:4,
+    image:"https://rukminim2.flixcart.com/image/612/612/jgsanww0/movie/g/d/a/2018-dvd-excel-home-videos-english-spider-man-home-coming-dvd-original-imaf4ybdkthsqzjy.jpeg?q=70" ,   
+    title: "Spider-Man Homecoming",
+    language:"English",
+    cost:700,
+},
+{
+    id:5,
+    image:"https://rukminim2.flixcart.com/image/612/612/jbi93m80/movie/m/t/b/2010-dvd-excel-home-videos-english-harry-potter-and-the-deathly-original-imafytaeztkhjm9s.jpeg?q=70" ,   
+    title: "Harry Potter- Dathly hollows",
+    language:"English",
+    cost:1200,
+},
+];
+export default movies;
